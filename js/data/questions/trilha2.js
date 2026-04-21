572a0b401e5452c0f12982d2e02f0e4ae2654c5e
ALESSANDRO BARBOSA DOS SANTOS - Prof Alê Chocolate <120406330+Prof-Ale@users.noreply.github.com>
2026-04-21 20:38:56 -0300

    Add arithmetic questions for educational module

--- js/data/questions/trilha2.js
+++ js/data/questions/trilha2.js
@@ -1,1 +1,44 @@
+// js/data/questions/trilha2.js
 
+export const trilha2 = [
+    {
+        t: 2, tipo: "aritmetica", bncc: "EF07MA01", bncc_desc: "Múltiplos e Divisores",
+        display: "Selecione um número que seja Múltiplo de 5:", 
+        botoes: ["3", "10", "7", "15"], 
+        res: ["10", "15"],
+        passo: "Os múltiplos de 5 terminam sempre em 0 ou 5.", 
+        dica: "Pense na tabuada do 5. Há mais do que uma opção correta!"
+    },
+    {
+        t: 2, tipo: "aritmetica", bncc: "EF07MA01", bncc_desc: "Múltiplos e Divisores",
+        display: "Qual destes números é Divisor de 12?", 
+        botoes: ["5", "3", "7", "4"], 
+        res: ["3", "4"],
+        passo: "12 pode ser dividido de forma exata por 1, 2, 3, 4, 6 e 12.", 
+        dica: "Que número vezes outro resulta em 12?"
+    },
+    {
+        t: 2, tipo: "aritmetica", bncc: "EF07MA01", bncc_desc: "Múltiplos e Divisores",
+        display: "Qual destes é Múltiplo de 3?", 
+        botoes: ["14", "9", "21", "16"], 
+        res: ["9", "21"],
+        passo: "A soma dos algarismos de um múltiplo de 3 está na tabuada do 3.", 
+        dica: "Lembre-se da regra prática de divisibilidade por 3!"
+    },
+    {
+        t: 2, tipo: "aritmetica", bncc: "EF07MA01", bncc_desc: "Múltiplos e Divisores",
+        display: "Selecione um Divisor de 20:", 
+        botoes: ["3", "4", "5", "6"], 
+        res: ["4", "5"],
+        passo: "20 é divisível por 1, 2, 4, 5, 10 e 20.", 
+        dica: "Quais números multiplicados dão 20?"
+    },
+    {
+        t: 2, tipo: "aritmetica", bncc: "EF07MA01", bncc_desc: "Números Primos",
+        display: "Qual destes é um Número Primo?", 
+        botoes: ["9", "11", "15", "13"], 
+        res: ["11", "13"],
+        passo: "Números primos só são divisíveis por 1 e por eles mesmos.", 
+        dica: "Cuidado com os números ímpares que têm mais divisores!"
+    }
+];
