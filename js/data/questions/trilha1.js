09cfa7d4dfb506fcee06d3944faeeb610cb4c473
ALESSANDRO BARBOSA DOS SANTOS - Prof Alê Chocolate <120406330+Prof-Ale@users.noreply.github.com>
2026-04-21 20:39:55 -0300

    Add questions data for Trilha 1

--- js/data/questions/trilha1.js
+++ js/data/questions/trilha1.js
@@ -1,1 +1,13 @@
-// Arquivo da Trilha 1
+// js/data/questions/trilha1.js
+
+export const trilha1 = [
+    {
+        t: 1, tipo: "reta", bncc: "EF07MA03", bncc_desc: "Soma e Subtração de Inteiros",
+        display: "-2 + 5 = ?", 
+        botoes: ["3", "-3", "7", "-7"], 
+        res: "3", 
+        passo: "Partindo do -2, andamos 5 casas para a direita, chegando ao 3 positivo.", 
+        dica: "Soma de positivo move para a <b>direita</b> na reta.",
+        a: -2, b: 5
+    }
+];
